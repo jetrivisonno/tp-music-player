a9006f371f1cb374bd398cd431824c54013cd2a6
tomkcey <tcaissie_dev@hotmail.com>
2019-05-13 12:33:55 -0400

    discogs file modified a little bit

--- tp_music/src/service/discogs.js
+++ tp_music/src/service/discogs.js
@@ -1,10 +1,12 @@
 const Discogs = require('disconnect').Client
-const discogs = new Discogs({ userToken: userToken })
+const discogs = new Discogs('TPUserAgent/1.0', { userToken: 'userToken' })
 
-discogs.database.search(params.query, { type: 'master', per_page: 2000 }, function (error, data) {
+discogs.database.search(params.query, { type: 'master', per_page: 20 }, function (err, data) {
+    if (err) throw err
     resultCallback(data)
 })
 
 discogs.database.getMaster(id, function (err, master) {
+    if (err) throw err
     resultCallback(master)
 })
