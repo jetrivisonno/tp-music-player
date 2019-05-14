6ccfd796f7f4581df9c9b3aff05b1dc9fdf09276
tomkcey <tcaissie_dev@hotmail.com>
2019-05-14 09:15:21 -0400

    working on the selection component, adding the presentation components

--- tp_music/src/component/selection-component.js
+++ tp_music/src/component/selection-component.js
@@ -1,9 +1,20 @@
-import React from 'react'
+// import React from 'react'
 
-const selection = ({ data }) => (
-    <li>
-        <img />
-        <p />
-        <p />
-    </li>
-)
+// function generateSuggestion (datas) {
+//     datas.map(function (data) {
+//         return (
+//             <li>
+//                 <img src={data.coverImg} />
+//                 <p>{data.title}</p>
+//             </li>
+//         )
+//     })
+// }
+
+// const selection = ({ data }) => (
+//     <li>
+//         <img src={data} />
+//         <p />
+//         <p />
+//     </li>
+// )
