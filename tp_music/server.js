100c4492ced884e9058e9223edee3ec404cea161
Thomas Caissie <tcaissie@isimtl.local>
2019-05-13 20:59:10 -0400

    got discogs working, now making container react for search bar

--- tp_music/server.js
+++ tp_music/server.js
@@ -9,6 +9,12 @@ const CONTENT_TYPE_JSON = 'application/json'
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
+app.get('/', (req, res) => {
+    // require the discog up top
+    // insert discog method to fetch info from api
+    // read the data
+})
+
 app.listen(PORT, () => {
     console.log('Listening on http://localhost:%s', PORT)
 })
--- tp_music/src/service/discogs.js
+++ tp_music/src/service/discogs.js
@@ -1,16 +1,12 @@
 const Discogs = require('disconnect').Client
-const discogs = new Discogs('TPUserAgent/1.0', { userToken: 'FlXtWCIhazFneRgHbghHuXhEaYbFfThLhhIhjQBB' })
+const dis = new Discogs('tc_jt_ie/1.0', { userToken: 'FlXtWCIhazFneRgHbghHuXhEaYbFfThLhhIhjQBB' })
 
-discogs.database.search(params.query, { type: 'master', per_page: 20 }, function (err, data) {
+dis.database().search('metal', { type: 'master', per_page: 5 }, function (err, data) {
     if (err) throw err
-    resultCallback(data)
-})
-
-discogs.database.getMaster(id, function (err, master) {
-    if (err) throw err
-    resultCallback(master)
-})
-
-function resultCallback (data) {
     console.log(data)
-}
+})
+
+dis.database().getMaster(106350, function (err, master) {
+    if (err) throw err
+    console.log(master)
+})
