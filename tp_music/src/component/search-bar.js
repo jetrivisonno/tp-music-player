5bcf865e9220709b530eac0baf5fcabe4b025616
Ismail Elamri <ismailelamri@gmail.com>
2019-05-14 16:01:21 -0400

    button

--- tp_music/src/component/search-bar.js
+++ tp_music/src/component/search-bar.js
@@ -3,7 +3,7 @@ import React from 'react'
 const SearchBar = ({ id, name, method }) => (
     <div>
         <input id={id} type='search' name={name} placeholder={name} onKeyPress={method} />
-        {/* insert button here */}
+        {<input type='submit' onClick={method} />}
     </div>
 )
 
