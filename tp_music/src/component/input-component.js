85d1bbaaeabd0266e2c7b134d217b187d935e7c2
Jonathan Trivisonno <jtriviso@isimtl.local>
2019-05-13 21:46:52 -0400

    test de search

--- tp_music/src/component/input-component.js
+++ tp_music/src/component/input-component.js
@@ -1,5 +1,11 @@
 import React from 'react'
 
+const search = require('../service/discogs.js')
+
+const doSomething = function () {
+    search.search()
+}
+
 const InputComponent = ({ type, id, name }) => (
     <div>
         <input
@@ -7,6 +13,7 @@ const InputComponent = ({ type, id, name }) => (
             id={id}
             name={name}
             placeholder={type}
+            onBlur={doSomething}
         />
     </div>
 )
--- tp_music/src/service/discogs.js
+++ tp_music/src/service/discogs.js
@@ -1,7 +1,7 @@
 const Discogs = require('disconnect').Client
 const dis = new Discogs('tc_jt_ie/1.0', { userToken: 'FlXtWCIhazFneRgHbghHuXhEaYbFfThLhhIhjQBB' })
 
-dis.database().search('metal', { type: 'master', per_page: 5 }, function (err, data) {
+const search = dis.database().search('metal', { type: 'master', per_page: 5 }, function (err, data) {
     if (err) throw err
     console.log(data)
 })
@@ -10,3 +10,7 @@ dis.database().getMaster(106350, function (err, master) {
     if (err) throw err
     console.log(master)
 })
+
+module.exports = {
+    search: search
+}
