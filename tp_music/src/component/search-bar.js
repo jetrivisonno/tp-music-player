da9bcec4489b9284988aa58bb8e9446211c93130
Thomas Caissie <tcaissie@isimtl.local>
2019-05-14 18:22:54 -0400

    submit input in search-bar was between brackets, removed brackets

--- tp_music/src/component/search-bar.js
+++ tp_music/src/component/search-bar.js
@@ -3,7 +3,7 @@ import React from 'react'
 const SearchBar = ({ id, name, method }) => (
     <div>
         <input id={id} type='search' name={name} placeholder={name} onKeyPress={method} />
-        {<input type='submit' onClick={method} />}
+        <input type='submit' onClick={method} />
     </div>
 )
 
