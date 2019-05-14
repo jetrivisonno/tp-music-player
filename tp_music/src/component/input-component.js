c9289b93bff6db85883a201ae2f215d8217f782a
Thomas Caissie <tcaissie@isimtl.local>
2019-05-13 22:57:40 -0400

    moved the logic in the container js file instead of component js file

--- tp_music/src/component/input-component.js
+++ tp_music/src/component/input-component.js
@@ -1,24 +1,6 @@
 import React from 'react'
 
-const search = require('../service/discogs.js')
-
-const searchbar = function () {
-    const input = document.getElementById('search_id')
-    // if (event.charCode === 13) {
-    //     search.search(event.target.value, { type: 'master', per_page: 5 }, function (err, data) {
-    //         if (err) throw err
-    //         console.log(data)
-    //     })
-    // }
-    if (event.charCode === 13) {
-        search.search(input.value, { type: 'master', per_page: 5 }, function (err, data) {
-            if (err) throw err
-            console.log(data)
-        })
-    }
-}
-
-const InputComponent = ({ type, id, name }) => (
+const InputComponent = ({ type, id, name, searchbar }) => (
     <div>
         <input
             type={type}
--- tp_music/src/container/search-container.js
+++ tp_music/src/container/search-container.js
@@ -1,6 +1,25 @@
 import React, { Component } from 'react'
 import InputComponent from 'component/input-component'
 
+const search = require('../service/discogs.js')
+const KEY_ENTER = 13 // in the ascii table, 13 is the carriage return key
+
+const searchbar = function () {
+    const input = document.getElementById('search_id')
+    // if (event.charCode === KEY_ENTER) {
+    //     search.search(event.target.value, { type: 'master', per_page: 5 }, function (err, data) {
+    //         if (err) throw err
+    //         console.log(data)
+    //     })
+    // }
+    if (event.charCode === KEY_ENTER) {
+        search.search(input.value, { type: 'master', per_page: 5 }, function (err, data) {
+            if (err) throw err
+            console.log(data)
+        })
+    }
+}
+
 class SearchContainer extends Component {
     render () {
         return (
@@ -8,6 +27,7 @@ class SearchContainer extends Component {
                 type='search'
                 id='search_id'
                 name='search'
+                searchbar={searchbar}
             />
         )
     }
