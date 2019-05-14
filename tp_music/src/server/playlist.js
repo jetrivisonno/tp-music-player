67bb820aa310f23374e1635230a8118033c2e538
tomkcey <tcaissie_dev@hotmail.com>
2019-05-14 07:43:24 -0400

    added playlist and track entities

---
+++ tp_music/src/server/playlist.js
@@ -0,0 +1,9 @@
+class Playlist {
+    constructor (id, title, creationDate) {
+        this.id = id
+        this.title = title
+        this.creationDate = creationDate
+    }
+}
+
+module.exports = Playlist
---
+++ tp_music/src/server/track.js
@@ -0,0 +1,11 @@
+class Track {
+    constructor (id, playlistId, title, url, masterId) {
+        this.id = id
+        this.playlistId = playlistId
+        this.title = title
+        this.url = url
+        this.masterId = masterId
+    }
+}
+
+module.exports = Track
