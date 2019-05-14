3d6bdc1539cdfa7ce86ed758039f4007931a2d96
Jonathan Trivisonno <jtriviso@isimtl.local>
2019-05-13 23:01:04 -0400

    test

--- tp_music/src/component/input-component.js
+++ tp_music/src/component/input-component.js
@@ -2,13 +2,6 @@ import React from 'react'
 
 const search = require('../service/discogs.js')
 
-<<<<<<< HEAD
-const searchbar = function (event) {
-    search.search(event.target.value, { type: 'master', per_page: 5 }, function (err, data) {
-        if (err) throw err
-        console.log(data)
-    })
-=======
 const searchbar = function () {
     const input = document.getElementById('search_id')
     // if (event.charCode === 13) {
@@ -23,7 +16,6 @@ const searchbar = function () {
             console.log(data)
         })
     }
->>>>>>> c929a01a9f7cc4f913d9b71466d973aa72aa2bd6
 }
 
 const InputComponent = ({ type, id, name }) => (
@@ -33,11 +25,7 @@ const InputComponent = ({ type, id, name }) => (
             id={id}
             name={name}
             placeholder={type}
-<<<<<<< HEAD
-            onClick={searchbar}
-=======
             onKeyPress={searchbar}
->>>>>>> c929a01a9f7cc4f913d9b71466d973aa72aa2bd6
         />
     </div>
 )
