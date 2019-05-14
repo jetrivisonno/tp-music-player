27994e067646471d0ca7f25676d0df2db536c342
tomkcey <tcaissie_dev@hotmail.com>
2019-05-14 10:43:37 -0400

    tried to play with app container but didnt succeed, this is a void push

--- tp_music/src/container/app-container.js
+++ tp_music/src/container/app-container.js
@@ -4,6 +4,13 @@ import SearchContainer from 'container/search-container'
 import SelectionContainer from 'container/selection-container'
 
 class AppContainer extends Component {
+    constructor () {
+        super()
+        this.state = {
+            data: [],
+            searchData: []
+        }
+    }
     render () {
         return (
             <div>
