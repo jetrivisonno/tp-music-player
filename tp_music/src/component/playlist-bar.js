5d5ee9739ca84d8a92b6652f4921c78c65223103
Thomas Caissie <tcaissie@isimtl.local>
2019-05-14 15:50:27 -0400

    implemented the playlist bar

---
+++ tp_music/src/component/playlist-bar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function generateOptions (datas) {
+    return datas.map(function (data, index) {
+        return (
+            <option key={index} value={index}>{data.text}</option>
+        )
+    })
+}
+
+const PlaylistBar = ({ id, name, options }) => (
+    <select id={id} name={name}>
+        {generateOptions(options)}
+    </select>
+)
+
+export default PlaylistBar
