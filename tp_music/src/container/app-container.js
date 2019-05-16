f5f2840b1d603040abc152757895a9fe8c1f62c4
Jonathan Trivisonno <jtriviso@isimtl.local>
2019-05-16 13:00:12 -0400

    commented out detailcontainer for now becaus eit was causing errors

--- tp_music/src/container/app-container.js
+++ tp_music/src/container/app-container.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 import SearchBar from 'component/search-bar'
 import PlaylistBar from '../component/playlist-bar'
 import SearchResult from '../component/search-result'
-import DetailContainer from 'detail-container'
+// import DetailContainer from '../detail-container'
 
 import '../css/main.css'
 
@@ -88,3 +88,5 @@ class AppContainer extends Component {
         )
     }
 }
+
+export default AppContainer
