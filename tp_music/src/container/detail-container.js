4fa0899a2de814ee20656055cfd5e41036269b1e
Jonathan Trivisonno <jtriviso@isimtl.local>
2019-05-16 12:54:29 -0400

    push start of detail container

---
+++ tp_music/src/container/detail-container.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react'
+
+class DetailContainer extends Component {
+    constructor () {
+        super()
+        this.state = {
+            master_id: ''
+        }
+    }
+
+    componentDidMount () {
+        this.setState({
+            master_id: true
+        })
+    }
+
+    render () {
+        return (
+            <div>
+                {/* */}
+            </div>
+        )
+    }
+}
+
+export default DetailContainer
